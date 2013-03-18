3a24cd9f5b69b1c10ecdb2e9064c4350e6790dca
Christian Neumann <x-ian@x-ian.net>
2013-03-18 12:40:39 +0100

    first version of independent WiFi Internet probe

---
+++ probes/TP-Link_Internet_Probe/create_message.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# ./create_message.sh heartbeat customer_id 2 1 2 20121022-135502 11 22 33 44
+#./create_message.sh heartbeat customer_id 2 1 2 20121022-135502 11 22 33 44
+
+MESSAGE_TYPE_ID=$1
+CUSTOMER_ID=$2
+PROBE_ID=$3
+OUTGOING_MESSAGE_COUNT=$4
+RESTART_COUNT=$5
+DEVICE_TIME=$6
+VALUE1=${7}
+VALUE2=${8}
+VALUE3=${9}
+VALUE4=${10}
+
+#URL=http://localhost:3000/messages/create_from_probe
+URL=http://www.marsmonitoring.com/messages/create_from_probe
+
+VALUES=\"message\":{\"data\":\"$MESSAGE_TYPE_ID,$CUSTOMER_ID,$PROBE_ID,$OUTGOING_MESSAGE_COUNT,$RESTART_COUNT,$DEVICE_TIME,$VALUE1,$VALUE2,$VALUE3,$VALUE4\"}
+
+#VALUES=\"message\":{\"data\":\"PAYLOAD,1,38,103,114,20111225-001412,14,16468,,\"}
+#VALUES=\"message\":{\"data\":\"PAYLOAD,2,5,103,114,20111225-001412,14,16468,,\"}
+
+#curl -v -H "Accept: application/json" -H "Content-type: application/json" -X POST -d " {`echo $VALUES`}" $URL
+wget -v --header "Accept: application/json" --header "Content-type: application/json" --post-data="{`echo $VALUES`}" $URL
---
+++ probes/TP-Link_Internet_Probe/mesure_network_speed.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# measure PING and HTTP speed and post to marsmonitoring.com
+
+TIME=$(date +%Y%m%d-%H%M%S)
+
+PING=$(ping -qc 10 google.com | awk -F/ '{ print $4 }' | tail -1)
+
+# wget -O /dev/null http://ipv4.download.thinkbroadband.com/10MB.zip 2>&1 | tail -2 | awk  '{ print $3 " " $4 }' | sed 's/[()]//g'
+# todo, check unit and convert if necessary from MB/s to KB/s
+HTTP=$(wget -O /dev/null http://ipv4.download.thinkbroadband.com/10MB.zip 2>&1 | tail -2 | awk  '{ print $3 " " $4 }' | sed 's/[()]//g')
+HTTP_SPEED=$(echo $HTTP | awk '{ print $1 }')
+HTTP_UNIT=$(echo $HTTP | awk '{ print $2 }')
+if [ "$HTTP_UNIT" == "MB/s" ]; then
+	HTTP_SPEED=$(awk 'BEGIN {OFMT="%.2f";print ("'"$HTTP_SPEED"'" * 1000) }')
+fi
+		
+/root/mars/create_message.sh heartbeat customer_id 11 1 1 $TIME $PING $HTTP_SPEED 
