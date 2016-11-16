da4e8cd00b19bb515960f57905d37b6f752a1c81
Christian Neumann <x-ian@x-ian.net>
2016-11-16 19:44:23 +0100

    pathes

--- probes/TP-Link_Internet_Probe/measure_network_speed_after_restart.sh
+++ probes/TP-Link_Internet_Probe/measure_network_speed_after_restart.sh
@@ -4,7 +4,7 @@
 
 TIME=$(date +%Y%m%d-%H%M%S)
 
-PING=$(ping -qc 10 google.com | awk -F/ '{ print $4 }' | tail -1)
+PING=$(ping -qc 10 google.com | awk -F/ '{ print $5 }' | tail -1)
 
 # wget -O /dev/null http://ipv4.download.thinkbroadband.com/10MB.zip 2>&1 | tail -2 | awk  '{ print $3 " " $4 }' | sed 's/[()]//g'
 # todo, check unit and convert if necessary from MB/s to KB/s
@@ -15,6 +15,6 @@ if [ "$HTTP_UNIT" == "MB/s" ]; then
 	HTTP_SPEED=$(awk 'BEGIN {OFMT="%.2f";print ("'"$HTTP_SPEED"'" * 1000) }')
 fi
 		
-/root/mars/create_message.sh heartbeat customer_id 11 1 1 $TIME $PING $HTTP_SPEED 
+$(dirname "$0")/create_message.sh restart customer_id 11 1 1 $TIME $PING $HTTP_SPEED 
 
 rm -f create_from_probe*
