88842db5a1f1819e8dfcbc9decda726e7e2da4e8
x-ian <x-ian@x-ian.net>
2017-02-11 15:40:04 +0100

    restart count before message

--- probes/Ubuntu_Internet_Probe/measure_network_speed_after_restart.sh
+++ probes/Ubuntu_Internet_Probe/measure_network_speed_after_restart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sleep 60
+sleep 30
 
 # measure PING and HTTP speed and post to marsmonitoring.com
 
 PATH=$PATH:/bin:/usr/bin
 source $(dirname "$0")/counter_restarts.txt
 
-$(dirname "$0")/measure_network_speed.sh restart
-
 ((RESTART_COUNTER++))
 echo "RESTART_COUNTER=$RESTART_COUNTER" > $(dirname "$0")/counter_restarts.txt
+
+$(dirname "$0")/measure_network_speed.sh restart
