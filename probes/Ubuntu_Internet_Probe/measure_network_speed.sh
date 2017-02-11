71364bc867cc3ebb09b37010e72ebc8e6f572bdf
Christian Neumann <x-ian@x-ian.net>
2017-02-11 15:03:53 +0100

    conversion

--- probes/Ubuntu_Internet_Probe/measure_network_speed.sh
+++ probes/Ubuntu_Internet_Probe/measure_network_speed.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # measure PING and HTTP speed and post to marsmonitoring.com
 
 PATH=$PATH:/bin:/usr/bin
@@ -14,7 +12,8 @@ TIME=$(date +%Y%m%d-%H%M%S)
 PING=$(ping -qc 10 google.com | awk -F/ '{ print $5 }' | tail -1)
 
 HTTP=$(wget -O /dev/null http://ipv4.download.thinkbroadband.com/10MB.zip 2>&1 | tail -2 | awk  '{ print $3 " " $4 }' | sed 's/[()]//g')
-HTTP_SPEED=$(echo $HTTP | awk '{ print $1 }')
+
+HTTP_SPEED=$(echo $HTTP | awk '{ print $1 }' | tr ',' '.')
 HTTP_UNIT=$(echo $HTTP | awk '{ print $2 }')
 if [ "$HTTP_UNIT" == "MB/s" ]; then
 	HTTP_SPEED=$(awk 'BEGIN {OFMT="%.2f";print ("'"$HTTP_SPEED"'" * 1000) }')
