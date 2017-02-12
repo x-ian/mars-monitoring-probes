339760b281e32fe819743c32a8536e7d03b67b6d
x-ian <x-ian@x-ian.net>
2017-02-12 09:21:35 +0100

    restart

--- probes/Ubuntu_Ping_Probe/checkDevicesOnline.sh
+++ probes/Ubuntu_Ping_Probe/checkDevicesOnline.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+	# if nothing, use heartbeat
+	MESSAGE_TYPE=heartbeat
+else
+	# use first param
+	MESSAGE_TYPE=$1
+fi
+
 PATH=$PATH:/bin:/usr/bin
 BASEDIR=$(dirname `realpath $0`)
 
 source $BASEDIR/config.txt
-source $BASEDIR/counter_restarts.txt
 source $BASEDIR/counter_outgoing_messages.txt
 
 TIME=$(date +%Y%m%d-%H%M%S)
@@ -18,7 +25,7 @@ PING_3=$?
 ping -qc 10 192.168.0.24
 PING_4=$?
 
-$BASEDIR/create_message.sh heartbeat $CUSTOMER_ID $PROBE_ID $OUTGOING_MESSAGE_COUNTER $RESTART_COUNTER $TIME $PING_1 $PING_2 $PING_3 $PING_4
+$BASEDIR/create_message.sh $MESSAGE_TYPE $CUSTOMER_ID $PROBE_ID $OUTGOING_MESSAGE_COUNTER $RESTART_COUNTER $TIME $PING_1 $PING_2 $PING_3 $PING_4
 
 rm -f create_from_probe*
 ((OUTGOING_MESSAGE_COUNTER++))
---
+++ probes/Ubuntu_Ping_Probe/checkDevicesOnline_after_restart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sleep 30
+
+# measure PING and HTTP speed and post to marsmonitoring.com
+
+PATH=$PATH:/bin:/usr/bin
+source $(dirname "$0")/counter_restarts.txt
+
+((RESTART_COUNTER++))
+echo "RESTART_COUNTER=$RESTART_COUNTER" > $(dirname "$0")/counter_restarts.txt
+
+$(dirname "$0")/checkDevicesOnline.sh restart
